5c269787baaeeb74604565a360cc05af2c12e32e
David Domkář <david@domkar.dev>
2023-02-28 11:24:18 +0100

    feat: setup prisma

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+// @ts-check
+/// <reference types="@prettier/plugin-pug/src/prettier" />
+
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  plugins: [require.resolve('@prettier/plugin-pug')],
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  semi: true,
+  pugSingleQuote: true,
+  trailingComma: 'all',
+  pugAttributeSeparator: 'none',
+};
