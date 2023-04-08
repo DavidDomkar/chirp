05e871a60c2dcce7306657d784d061d960ec91dd
David Domkář <david@domkar.dev>
2023-04-08 20:06:33 +0200

    feat: make footer responsive

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,4 +1,7 @@
 <template lang="pug">
-nav.card.card-compact.shadow-2xl.rounded-b-none.bg-base-100.w-full.sticky.bottom-0.z-10
-  .card-body.text-center {{ new Date().getFullYear() }} &copy; David Domkář - Studentský zápočtový projekt
+.flex.w-full.justify-center.left-0.bottom-0.right-0.z-10.fixed.px-8
+  nav.card.card-compact.shadow-2xl.rounded-b-none.bg-base-100.max-w-full(
+    class='w-[1280px]'
+  )
+    .card-body.text-center {{ new Date().getFullYear() }} &copy; David Domkář - Studentský zápočtový projekt
 </template>
---
+++ error.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+definePageMeta<{
+  layout: 'default';
+}>();
+
+defineProps<{
+  error: any;
+}>();
+</script>
+
+<template lang="pug">
+.flex.justify-center.bg-base-300
+  .max-w-full.min-h-screen.flex.flex-col.px-8(class='w-[1344px]')
+    Header
+    .w-full.flex.flex-col.items-center.py-8.gap-8
+      h1.text-4xl.font-bold.text-center {{ error.statusCode }}
+      h2.text-xl.font-bold.text-center {{ error.message }}
+      NuxtLink.btn.btn-primary.w-48.m-auto(to='/') Go Home
+    Footer
+</template>
