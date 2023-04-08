ae2cf9fd15940bc7a021fcc276778d3889273614
David Domkář <david@domkar.dev>
2023-04-08 19:49:14 +0200

    feat: add footer

---
+++ components/Footer.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+nav.card.card-compact.shadow-2xl.rounded-b-none.bg-base-100.w-full.sticky.bottom-0.z-10
+  .card-body.text-center {{ new Date().getFullYear() }} &copy; David Domkář - Studentský zápočtový projekt
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,7 @@ const onSignOut = async () => {
 <template lang="pug">
 .flex.justify-center.bg-base-300
   .max-w-full.min-h-screen.flex.flex-col.px-8(class='w-[1344px]')
-    AppBar
+    Header
     slot
+    Footer
 </template>
